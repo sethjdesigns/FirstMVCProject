ef451f5160a555e5c738ab182ee165e2f1f487f6
Seth Johnson <sethjdesigns@gmail.com>
2015-03-30 14:24:37 -0400

    Created Unit Test

---
+++ WebAppTest/Class1.cs
@@ -0,0 +1,25 @@
+﻿using FirstMVCProject.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Xunit;
+
+namespace WebAppTest
+{
+    public class Class1
+    {
+        [Fact]
+
+        public void SomeTest()
+        {
+            var homeController = new HomeController();
+            var viewController = homeController.About();
+            var theView = (ViewResult)viewController;
+            Assert.NotNull(theView);
+        }
+
+    }
+}
