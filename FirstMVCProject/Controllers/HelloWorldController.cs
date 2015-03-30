476a075502ab37157546e62f11dbd8ba157ee028
Seth Johnson <sethjdesigns@gmail.com>
2015-03-30 15:36:43 -0400

    Change string return to view() ActionResult

--- FirstMVCProject/Controllers/HelloWorldController.cs
+++ FirstMVCProject/Controllers/HelloWorldController.cs
@@ -8,17 +8,11 @@ namespace FirstMVCProject.Controllers
 {
     public class HelloWorldController : Controller
     {
+
         // GET: HelloWorld
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>Default</b> action...";
-        }
-
-        // GET: HelloWorld/Welcome/
-
-        public string Welcome(string name, int ID = 1) 
-        {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            return View();
         }
 
 
