58c338c8959a92a64a71e467adb88b7be8b811e4
Seth Johnson <sethjdesigns@gmail.com>
2015-03-30 15:54:45 -0400

    Added back welcome method with query strings

--- FirstMVCProject/Controllers/HelloWorldController.cs
+++ FirstMVCProject/Controllers/HelloWorldController.cs
@@ -15,6 +15,14 @@ namespace FirstMVCProject.Controllers
             return View();
         }
 
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
 
     }
 }
