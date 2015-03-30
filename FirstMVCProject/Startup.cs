66d353cff0dfa35c5ee4f584b563c0c3742bea95
Seth Johnson <sethjdesigns@gmail.com>
2015-03-30 10:06:26 -0400

    Init FirstMVCProject

---
+++ FirstMVCProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstMVCProject.Startup))]
+namespace FirstMVCProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
