a5bf206a1b25e7d4c2d962fef44c5b09afad2ffa
Seth Johnson <sethjdesigns@gmail.com>
2015-04-03 10:07:18 -0400

    added validation attr's to Movie object

--- FirstMVCProject/Models/Movie.cs
+++ FirstMVCProject/Models/Movie.cs
@@ -7,17 +7,25 @@ namespace FirstMVCProject.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
         [Display(Name = "Release Date:")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         [Display(Name = "Genre:")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
         [Display(Name = "Price:")]
-        [DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = false)]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
         [Display(Name = "Rating:")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 
