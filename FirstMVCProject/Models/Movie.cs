6c383dda21187d4d80bbb13bc1a8199f639ad3c9
Seth Johnson <sethjdesigns@gmail.com>
2015-04-03 10:48:12 -0400

    cleaned up data attrs

--- FirstMVCProject/Models/Movie.cs
+++ FirstMVCProject/Models/Movie.cs
@@ -9,23 +9,13 @@ namespace FirstMVCProject.Models
         public int ID { get; set; }
         [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
-        [Display(Name = "Release Date:")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date:"),DataType(DataType.Date),DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
-        [Display(Name = "Genre:")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
-        [Required]
-        [StringLength(30)]
+        [Display(Name = "Genre:"),RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"),Required,StringLength(30)]
         public string Genre { get; set; }
-        [Display(Name = "Price:")]
-        [DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = false)]
-        [Range(1, 100)]
-        [DataType(DataType.Currency)]
+        [Display(Name = "Price:"),DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = false),Range(1, 100),DataType(DataType.Currency)]
         public decimal Price { get; set; }
-        [Display(Name = "Rating:")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
-        [StringLength(5)]
+        [Display(Name = "Rating:"),RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"),StringLength(5)]
         public string Rating { get; set; }
     }
 
