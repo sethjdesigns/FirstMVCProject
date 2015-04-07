c1987ec06beab546debd751ca70e1191e1244ecb
Seth Johnson <sethjdesigns@gmail.com>
2015-04-07 10:25:38 -0400

    Updated Bind methods
    Now includes the Binding methods

--- FirstMVCProject/Controllers/MoviesController.cs
+++ FirstMVCProject/Controllers/MoviesController.cs
@@ -84,7 +84,7 @@ namespace FirstMVCProject.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,RatingsEnum")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -116,7 +116,7 @@ namespace FirstMVCProject.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,RatingsEnum")] Movie movie)
         {
             if (ModelState.IsValid)
             {
